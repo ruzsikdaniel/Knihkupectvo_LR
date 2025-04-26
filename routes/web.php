a92400cb0d3c58ef8ffd13bf8b7995962f3e46ad
MariiaLytvynova <litmarrom@gmail.com>
2025-04-26 22:46:20 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,19 @@
 
 use App\Http\Controllers\ProfileController; //we created a home controller
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
+
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'show']
+// function () {
+//     return view('welcome');
+// }
+);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [UserController::class, 'show_logged'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -27,3 +30,7 @@ Route::post('/logout', function () {
     Auth::logout();
     return redirect('/');
 })->name('logout');
+
+Route::get('book_search', [BookController::class, 'book_search'])->middleware(['auth', 'find']); //check if the user is admin
+
+Route::get('findbook', [BookController::class, 'findbook']); //if the user is admin
