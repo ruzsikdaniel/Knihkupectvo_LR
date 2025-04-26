b1cbc06dfe9df7e6f608bd44e40b65e8b4724ab3
MariiaLytvynova <litmarrom@gmail.com>
2025-04-26 14:49:50 +0200

    Update header.blade.php - log out
    Add logout option for the big screen

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,14 +13,15 @@
         </div>
         <div class="col-lg-4 d-flex justify-content-center align-items-center gap-3">
         @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
+        <nav class="flex items-center justify-end gap-4">
                     @auth
-                        <a
-                            href="{{ url('/dashboard') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                        >
-                            Dashboard
-                        </a>
+                    <form method="POST" action="{{ route('logout') }}">
+                     @csrf
+                        <button>
+                        Log out
+                        </button>
+                    </form>
+
                     @else
                         <a
                             href="{{ route('login') }}"
