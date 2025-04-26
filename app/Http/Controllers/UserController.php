a0f4d94758e357112dc1e474e7bc2506b3557c69
MariiaLytvynova <litmarrom@gmail.com>
2025-04-26 18:59:46 +0200

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book; //get the book model
+
 
 class UserController extends Controller
 {
-    //
+    public function show(){
+        $book = Book::paginate(10);
+        return view('welcome', compact('book'));
+    }
 }
