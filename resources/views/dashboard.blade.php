f7011f8529a564bfca53ab14176e28562008b11b
MariiaLytvynova <litmarrom@gmail.com>
2025-04-26 23:50:31 +0200

    Add details of the book

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -35,9 +35,11 @@
                     @foreach($book as $books)
                         <li class="book-item">
                             <span class="d-flex flex-column border p-2">
-                                <img src="https://mrtns.sk/tovar/_l/2828/l2828965.jpg?v=17433079752" alt="Obálka knihy" class="img-fluid mb-2">
+                                <a href="{{route('book_det', $books->id)}}">
+                                    <img src="https://mrtns.sk/tovar/_l/2828/l2828965.jpg?v=17433079752" alt="Obálka knihy" class="img-fluid mb-2">
+                                </a>
                                 <div class="d-flex flex-column text-start mb-2">
-                                    <p id="item-title" class="mb-1 fw-bold"><a href="/html/item.html"
+                                    <p id="item-title" class="mb-1 fw-bold"><a href="{{route('book_det', $books->id)}}"
                                                                             class="text-decoration-none">{{$books->name}}</a></p>
                                     <p class="text-muted mb-0">{{$books->autor}}</p>
                                 </div>
