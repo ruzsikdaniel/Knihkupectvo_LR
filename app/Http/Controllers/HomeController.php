aaedebd62b28d5e03a20b59ebf0ce41b33e4aed4
MariiaLytvynova <litmarrom@gmail.com>
2025-04-28 14:50:02 +0200

    Add files via upload

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book; //get the book model
+use App\Models\Category; //get the book model
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        $book = Book::paginate(10);
+        $category = Category::query()->distinct()->pluck('name'); //get the 5 categories
+        return view('admin.index', compact('book', 'category'));
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book; //get the book model
+use App\Models\Category; //get the book model
 
 
 class UserController extends Controller
 {
-    public function show(){
+    public function show(){ //just main page of the unlogged user
         $book = Book::paginate(10);
-        return view('welcome', compact('book'));
+        $category = Category::query()->distinct()->pluck('name'); //get the 5 categories
+
+        //$category = Category::take(5)->get(); //get the 5 categories
+        //$genres = Book::select('genre')
+        return view('welcome', compact('book', 'category'));
     }
-    public function show_logged(){
+    public function show_logged(){ //logged user main
+        $category = Category::query()->distinct()->pluck('name'); //get the 5 categories
         $book = Book::paginate(10);
-        return view('dashboard', compact('book'));
+        return view('dashboard', compact('book', 'category'));
     }
 
     
