9e2561b47d488863ab4c859ac9cbd91a0fcd60b5
DánielRuzsik <xruzsik@stuba.sk>
2025-05-03 12:18:17 +0200

    about us and contacts link deleted from footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,17 +1,11 @@
 <footer class="container-fluid">
-    <article class="d-none d-lg-flex justify-content-center align-items-center">
-        <section class="col-lg-3">
-            <div id="panel-info" class="d-lg-flex justify-content-around align-items-center">
-                <a id="aboutus" class="mx-3">O nás</a>
-                <a id="contacts" class="mx-3">Kontakt</a>
-            </div>
+    <article class="d-none d-lg-flex align-items-center">
+        <section id="authors">
+            <p id="authors-text">
+                Maria Lytvynova, Dániel Ruzsik - WTECH 2025
+            </p>
         </section>
-        <section class="col-lg-6">
-            <div id="authors">
-                <p id="authors-text">Maria Lytvynova, Dániel Ruzsik - WTECH 2025</p>
-            </div>
-        </section>
-        <section class="col-lg-3 d-flex justify-content-around">
+        <section id="backToTop">
             <a href="#top">
                 <button id="btnBackToTop">
                     Späť ▲
@@ -20,12 +14,9 @@
         </section>
     </article>
 
-    <article class="d-lg-none d-flex justify-content-between">
-        <section class="d-flex">
-            <div id="panel-info" class="d-lg-flex flex-row justify-content-around align-items-center">
-                <a id="aboutus" class="mx-3">O nás</a>
-                <a id="contacts" class="mx-3">Kontakt</a>
-            </div>
+    <article class="d-lg-none d-flex justify-content-between align-items-center">
+        <section id="authors">
+            <p id="authors-text">Maria Lytvynova, Dániel Ruzsik - WTECH 2025</p>
         </section>
         <section>
             <a href="#top">
